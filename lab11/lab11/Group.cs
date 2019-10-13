ba1c5a749dc2dd44554c72b0d88901e5cf54bba9
Snoop593 <45264405+Snoop593@users.noreply.github.com>
2019-10-13 15:59:35 +0300

    Add files via upload

---
+++ lab11/lab11/Group.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace l1
+{
+
+    public class Group
+    {
+        public string name { get; set; }
+        public string[] header { get; set; }
+        public List<Student> students { get; set; }
+        public Dictionary<string, double> subjects { get; set; }
+        public Dictionary<string, double> averageMarks { get; set; }
+
+
+        public Group(List<string[]> data, string name = "ИП-41"){
+            this.name = name;
+            header = data[0];
+            data.Remove(data[0]);
+
+            subjects = new Dictionary<string, double>();
+            averageMarks = new Dictionary<string, double>();
+            students = new List<Student>();
+
+
+            for (int i = 3; i < header.Length; i++) {
+                subjects.Add(header[i], 0);
+                averageMarks.Add(header[i], 0);
+            }
+
+            subjects.Add("Средний", 0);
+            averageMarks.Add("Средний", 0);
+
+            foreach (string[] line in data){
+                Student student = new Student(line[0], line[1], line[2]);
+                for (int i = 3; i < header.Length; i++) {
+                    Subject subj = new Subject(header[i], double.Parse(line[i]));
+                    student.AddSubj(subj);
+                    subjects[header[i]] += double.Parse(line[i]);
+                    averageMarks[header[i]] = subjects[header[i]] / (students.Count + 1);
+                }
+
+                subjects["Средний"] += student.averageMark;
+                averageMarks["Средний"] = subjects["Средний"] / (students.Count + 1);
+                students.Add(student);
+            }
+        }
+    }
+}
---
+++ lab11/lab11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace l1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+                Task.Run(args[0], args[1], args[2]);
+            }
+            catch (Exception ex){
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine(".....");
+            Console.ReadKey();
+        }
+    }
+}
